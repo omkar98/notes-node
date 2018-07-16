f52ce1a23fc1c75dbee3e98827130c443f8f43c5
omkar98 <edu.omkar@gmail.com>
2018-07-16 08:11:48 +0530

    Objective - 6: Getting 'Simplified' Input from the user from the Command Line using a third party module named 'yargs'.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-Objective - 5: Getting Input from the user from the Command Line and using this input for our application. */
+Objective - 6: Getting 'Simplified' Input from the user from the Command Line using a third party module named 'yargs'. */
 
 console.log('Loading App...');
 
@@ -7,10 +7,13 @@ console.log('Loading App...');
 const _ = require('lodash'); 
 const fs = require('fs');
 //const os = require('os');
+const yargs = require('yargs');
 
 /*Self-Defined Modules*/
 const notes = require('./notes.js');
 
+
+const argv = yargs.argv;
 var command = process.argv[2];
 /*
 console.log(process.argv)
@@ -20,22 +23,26 @@ argv stores all the command line arguments passed in the cmd prompt..
   We choose index '2', because the arguments passed from the command list are stored from index 2(i.e in the 3rd position). The first 2 indices store the File paths. To check the arguments, go ahead and print: console.log(process.argv) 
 */
 console.log('Command: ', command);
+console.log('Process: ', process.argv); 
+console.log('Yargs: ', argv);
 
 if(command === 'add')
 {
     console.log('Adding New note');
+    /*Here basically, we have created our own module 'notes.js' and defined a function named 'addNote' there.*/
+    notes.addNote(argv.title, argv.body);
 }
 else if(command === 'list')
 {
-    console.log('Listing all notes');
+    notes.getAll();
 }
 else if(command === 'read')
 {
-    console.log('Reading the note');
+    notes.getNote(argv.title);
 }
 else if(command === 'remove')
 {
-    console.log('Remobing the note');
+    notes.removeNote(argv.title);
 }
 else
 {
--- notes.js
+++ notes.js
@@ -4,7 +4,31 @@ console.log('Loading Notes.js');
 View exports - console.log(module);*/
 
 /*This is an error function syntax in JS. This ' () => ' converts a regular function to an error function.*/
-module.exports.addNote = () => {
+/*module.exports.addNote = () => {
     console.log('Add Note');
     return 'New Note';
+};*/
+/*Here we create a variable named addnote that holds the value retured from the the function, taking title and body as an argument.*/
+var addNote = (title, body) => {
+    console.log('Adding Note: ', title, body);
+};
+
+var getAll = () => {
+    console.log('Listing all notes');
+};
+
+var getNote = (title) => {
+    console.log('Reading Note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing Note', title);
+};
+
+/*insteading of exporting each and every function, we define a comman module.exports function that holds all the functions needed to be exported.*/
+module.exports = {
+    addNote,  //addNote: addNote === addNote
+    getAll,
+    getNote,
+    removeNote
 };
