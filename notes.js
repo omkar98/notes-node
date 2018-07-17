b6ebbac1b3d824d61db2f00422b5f515e995edf5
omkar98 <edu.omkar@gmail.com>
2018-07-17 07:41:50 +0530

    Objective - 8: a) The user entered strings (from the command-line) should be stored in a file(a json file, so that it becomes easier to manipulate the data.)
    - To store in a file, we need to convert these strings in an object and fetch the required data, as per our requirements and store them back to a file, in string format.
    - Steps followed are:
    Whenever the function addNote is called:
     - We create a local object 'note' and store the user typed 'title' and 'body' into the local's 'title' and 'body' attributes.
     - We now push this 'note' data into the 'notes' array.
     - This data of 'notes' array in now stored into a file 'notes-data.json' in a string format.
    
    Issues:
    The function fs.writeFileSync( ) over-writes the previous data with the new one. Hence, we cannot store more than 1 notes.

--- notes.js
+++ notes.js
@@ -9,9 +9,29 @@ View exports - console.log(module);*/
     return 'New Note';
 };*/
 /*Here we create a variable named addnote that holds the value retured from the the function, taking title and body as an argument.*/
+
+/*Our target:
+a) The user entered strings (from the command-line) should be stored in a file(a json file, so that it becomes easier to manipulate the data.)  
+b) To store in a file, we need to convert these strings in an object and fetch the required data, as per our requirements and store them back to a file, in string format.
+*/
+var fs = require('fs');
+
+/* Whenever the function addNote is called:
+ - We create a local object 'note' and store the user typed 'title' and 'body' into the local's 'title' and 'body' attributes.
+ - We now push this 'note' data into the 'notes' array.
+ - This data of 'notes' array in now stored into a file 'notes-data.json' in a string format. */
+
 var addNote = (title, body) => {
-    console.log('Adding Note: ', title, body);
+   var notes = [];
+   var note = {
+       title, //title = title
+       body //body = body
+   };
+   notes.push(note);
+   fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
+
+
 
 var getAll = () => {
     console.log('Listing all notes');
